cbead212c8b47832ceee75f19abbc6cf041ce1ef
marco bottichio <marco.bottichio@emea.nttdata.com>
2023-06-03 11:03:29 +0200

    feat: added lints

---
+++ test/features/user_account/domain/repositories/user_repository_mock.dart
@@ -0,0 +1,4 @@
+import 'package:mockito/mockito.dart';
+import 'package:trip_planner/features/user_account/domain/repositories/user_repository.dart';
+
+class MockUserRepository extends Mock implements UserRepository {}
---
+++ test/features/user_account/domain/usecases/listen_user_test.dart
@@ -0,0 +1,5 @@
+import 'package:trip_planner/features/user_account/domain/usecases/listen_user.dart';
+
+void main() {
+  ListenUser usecase;
+}
