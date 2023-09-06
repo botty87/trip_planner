4bc7a060b056b7b08f7927256b6d8579f5f455c9
marco bottichio <marco.bottichio@pm.me>
2023-09-06 22:18:00 +0200

    fix: fixed time picker

--- lib/features/trip_stops/presentation/widgets/new_edit_trip_stop_form/time_widget.dart
+++ lib/features/trip_stops/presentation/widgets/new_edit_trip_stop_form/time_widget.dart
@@ -9,93 +9,108 @@ class _TimeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(label, style: Theme.of(context).textTheme.labelLarge),
-        const SizedBox(width: HORIZONTAL_SPACE_S),
-        StreamBuilder<TimeOfDay?>(
-            stream: time,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return TextButton(
-                  key: Key('startTimeButton'),
-                  onPressed: () {
-                    _showTimePicker(context, snapshot.data);
-                  },
-                  child: Text(snapshot.data!.toFormattedString()),
-                );
-              } else {
-                return TextButton(
-                  key: Key('startTimeButton'),
-                  onPressed: () {
-                    onTimeChanged(TimeOfDay.now());
-                    _showTimePicker(context, null);
-                  },
-                  child: Text(LocaleKeys.setTime.tr()),
-                );
-              }
-            }),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text(label, style: Theme.of(context).textTheme.labelLarge),
+          const SizedBox(width: HORIZONTAL_SPACE_S),
+          StreamBuilder<TimeOfDay?>(
+              stream: time,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return TextButton(
+                    key: Key('startTimeButton'),
+                    onPressed: () {
+                      _showTimePicker(context, snapshot.data);
+                    },
+                    child: Text(snapshot.data!.toFormattedString()),
+                  );
+                } else {
+                  return TextButton(
+                    key: Key('startTimeButton'),
+                    onPressed: () {
+                      _showTimePicker(context, null);
+                    },
+                    child: Text(LocaleKeys.setTime.tr()),
+                  );
+                }
+              }),
+        ],
+      ),
     );
   }
 
   void _showTimePicker(BuildContext context, TimeOfDay? _currentTime) {
-    final initialTime = _currentTime ?? TimeOfDay.now();
+    TimeOfDay selectedTime = _currentTime ?? TimeOfDay.now();
 
-    getPicker() {
-      return showPicker(
-        isInlinePicker: Platform.isIOS,
-        context: context,
-        value: Time(hour: initialTime.hour, minute: initialTime.minute),
-        duskSpanInMinutes: 120,
-        is24HrFormat: true,
-        height: 350,
-        onChange: (value) {
-          if (Platform.isIOS) {
-            final timeOfDay = TimeOfDay(hour: value.hour, minute: value.minute);
-            onTimeChanged(timeOfDay);
-          }
-        },
-        iosStylePicker: Platform.isIOS,
-        hourLabel: LocaleKeys.hours.tr(),
-        minuteLabel: LocaleKeys.minutes.tr(),
-        cancelText: LocaleKeys.cancel.tr(),
-        isOnChangeValueMode: Platform.isIOS,
-        dialogInsetPadding: EdgeInsets.all(0),
+    Widget getPicker() {
+      return Column(
+        children: [
+          showPicker(
+            isInlinePicker: true,
+            context: context,
+            value: Time(hour: selectedTime.hour, minute: selectedTime.minute),
+            duskSpanInMinutes: 120,
+            is24HrFormat: true,
+            height: 350,
+            onChange: (value) => selectedTime = TimeOfDay(hour: value.hour, minute: value.minute),
+            iosStylePicker: true,
+            hourLabel: LocaleKeys.hours.tr(),
+            minuteLabel: LocaleKeys.minutes.tr(),
+            cancelText: LocaleKeys.cancel.tr(),
+            isOnChangeValueMode: true,
+            dialogInsetPadding: EdgeInsets.all(0),
+          ),
+          const SizedBox(height: VERTICAL_SPACE_S),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              const SizedBox(width: HORIZONTAL_SPACE_L),
+              IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {
+                  onTimeChanged(selectedTime);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        ],
+      );
+    }
+
+    Widget _getModal() {
+      return Container(
+        height: 440,
+        padding: const EdgeInsets.only(top: 6.0),
+        // The bottom margin is provided to align the popup above the system
+        // navigation bar.
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        // Provide a background color for the popup.
+        color: Theme.of(context).scaffoldBackgroundColor,
+        // Use a SafeArea widget to avoid system overlaps.
+        child: SafeArea(
+          top: false,
+          child: getPicker(),
+        ),
       );
     }
 
     if (Platform.isIOS) {
       showCupertinoModalPopup<void>(
-        context: context,
-        builder: (BuildContext context) => Container(
-          height: 400,
-          padding: const EdgeInsets.only(top: 6.0),
-          // The bottom margin is provided to align the popup above the system
-          // navigation bar.
-          margin: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-          // Provide a background color for the popup.
-          color: CupertinoColors.systemBackground.resolveFrom(context),
-          // Use a SafeArea widget to avoid system overlaps.
-          child: SafeArea(
-            top: false,
-            child: getPicker(),
-          ),
-        ),
-      );
+          context: context, builder: (BuildContext context) => _getModal());
     }
 
     if (Platform.isAndroid) {
-      Navigator.of(context).push(getPicker()).then((value) {
-        assert(value is Time?);
-        final time = value as Time?;
-        if (time != null) {
-          final timeOfDay = TimeOfDay(hour: time.hour, minute: time.minute);
-          onTimeChanged(timeOfDay);
-        }
-      });
+      showBottomSheet(context: context, builder: (context) => _getModal());
     }
   }
 }
