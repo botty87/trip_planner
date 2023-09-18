0775ef976fb41707477703f88b0988cc9139660b
marco bottichio <marco.bottichio@pm.me>
2023-09-18 22:59:22 +0200

    fix: run dart fix

--- lib/features/day_trips/presentation/pages/day_trip_page.dart
+++ lib/features/day_trips/presentation/pages/day_trip_page.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
