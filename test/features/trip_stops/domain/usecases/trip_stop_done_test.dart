b004d704c33ff89ceb147d4a8b8f8813955b6f49
marco bottichio <marco.bottichio@pm.me>
2023-10-15 23:30:02 +0200

    fix: fixes on imports

--- test/features/trip_stops/domain/usecases/trip_stop_done_test.dart
+++ test/features/trip_stops/domain/usecases/trip_stop_done_test.dart
@@ -1,8 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mockito/mockito.dart';
-import 'package:trip_planner/features/trip_stops/domain/usecases/create_trip_stop.dart';
 import 'package:trip_planner/features/trip_stops/domain/usecases/trip_stop_done.dart';
 import 'package:trip_planner/features/trip_stops/errors/trip_stops_failure.dart';
 
