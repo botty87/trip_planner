633f817c17ee703b54bcd5905be3c9d31d845137
marco bottichio <marco.bottichio@pm.me>
2023-11-27 18:42:38 +0100

    feat: added responsive framework

--- lib/core/my_app.dart
+++ lib/core/my_app.dart
@@ -1,12 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'di/di.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import '../features/user_account/presentation/cubit/user/user_cubit.dart';
+import 'di/di.dart';
 import 'routes/app_router.dart';
 import 'routes/app_router.gr.dart';
-
 
 class MyApp extends StatelessWidget {
   final ThemeData theme;
@@ -42,6 +42,29 @@ class MyApp extends StatelessWidget {
           supportedLocales: context.supportedLocales,
           locale: context.locale,
           scrollBehavior: const MaterialScrollBehavior().copyWith(scrollbars: false),
+          builder: (context, child) => ResponsiveBreakpoints.builder(
+            child: Builder(
+              builder: (context) {
+                return ResponsiveScaledBox(
+                  width: ResponsiveValue<double>(
+                    context,
+                    conditionalValues: [
+                      Condition.equals(name: MOBILE, value: 450),
+                      Condition.between(start: 800, end: 1100, value: 800),
+                      Condition.between(start: 1000, end: 1200, value: 700),
+                    ],
+                  ).value,
+                  child: child!,
+                );
+              },
+            ),
+            breakpoints: [
+              const Breakpoint(start: 0, end: 450, name: MOBILE),
+              const Breakpoint(start: 451, end: 800, name: TABLET),
+              const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+              const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+            ],
+          ),
         ),
       ),
     );
