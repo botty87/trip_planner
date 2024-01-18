a9b96e05ec2593b91a2166bcfcf3bb87ae2f34e5
marco bottichio <marco.bottichio@pm.me>
2024-01-18 14:22:02 +0100

    fixed wrong padding
    fixed wrong padding on tripstops list on discover and when device > mobile

--- lib/features/discover_new_trips/presentation/pages/discover_new_trip_stops_page.dart
+++ lib/features/discover_new_trips/presentation/pages/discover_new_trip_stops_page.dart
@@ -3,6 +3,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
 
 import '../../../../core/constants.dart';
 import '../../../../core/di/di.dart';
--- lib/features/discover_new_trips/presentation/widgets/trip_stops/list/discover_new_trip_stops_list.dart
+++ lib/features/discover_new_trips/presentation/widgets/trip_stops/list/discover_new_trip_stops_list.dart
@@ -11,40 +11,55 @@ class _DiscoverNewTripStopsList extends HookWidget with TripStopStartEndTimeMixi
   Widget build(BuildContext context) {
     useAutomaticKeepAlive();
 
-    final tripStops = context.select((DiscoverNewTripStopsCubit cubit) => cubit.state.maybeMap(
-          loaded: (state) => state.tripStops,
-          orElse: () => throw Exception('Unexpected state'),
-        ));
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final tripStops = context.select((DiscoverNewTripStopsCubit cubit) => cubit.state.maybeMap(
+              loaded: (state) => state.tripStops,
+              orElse: () => throw Exception('Unexpected state'),
+            ));
 
-    final DateTime dayTripStartDateTime = DateTime.now().copyWith(
-      hour: _dayTrip.startTime.hour,
-      minute: _dayTrip.startTime.minute,
-    );
+        final DateTime dayTripStartDateTime = DateTime.now().copyWith(
+          hour: _dayTrip.startTime.hour,
+          minute: _dayTrip.startTime.minute,
+        );
 
-    //this is needed to avoid the recalculation of the trip start and end times on every build
-    final List<Pair<DateTime, DateTime>> tripStopStartEndTimes = [];
+        //this is needed to avoid the recalculation of the trip start and end times on every build
+        final List<Pair<DateTime, DateTime>> tripStopStartEndTimes = [];
 
-    return ListView.separated(
-      padding: defaultPagePadding,
-      itemCount: tripStops.length,
-      itemBuilder: (context, index) {
-        final tripStop = tripStops[index];
-        if (tripStopStartEndTimes.length <= index) {
-          tripStopStartEndTimes.add(getTripStartEndTimes(
-            tripStops: tripStops,
-            tripStopStartEndTimes: tripStopStartEndTimes,
-            dayTripStartDateTime: dayTripStartDateTime,
-            currentIndex: index,
-          ));
-        }
+        final maxWidth = ResponsiveBreakpoints.of(context).largerThan(MOBILE)
+            ? constraints.maxWidth * 0.8
+            : constraints.maxWidth;
 
-        return _TripStopCard(
-          tripStop: tripStop,
-          tripStartEndTimes: tripStopStartEndTimes[index],
-          context: context,
+        final horizantalPadding = (constraints.maxWidth - maxWidth) / 2 + pageHorizontalPadding;
+
+        return ListView.separated(
+          padding: EdgeInsets.fromLTRB(
+            horizantalPadding,
+            verticalSpace,
+            horizantalPadding,
+            verticalSpace,
+          ),
+          itemCount: tripStops.length,
+          itemBuilder: (context, index) {
+            final tripStop = tripStops[index];
+            if (tripStopStartEndTimes.length <= index) {
+              tripStopStartEndTimes.add(getTripStartEndTimes(
+                tripStops: tripStops,
+                tripStopStartEndTimes: tripStopStartEndTimes,
+                dayTripStartDateTime: dayTripStartDateTime,
+                currentIndex: index,
+              ));
+            }
+
+            return _TripStopCard(
+              tripStop: tripStop,
+              tripStartEndTimes: tripStopStartEndTimes[index],
+              context: context,
+            );
+          },
+          separatorBuilder: (context, index) => _TravelCard(tripStop: tripStops[index]),
         );
       },
-      separatorBuilder: (context, index) => _TravelCard(tripStop: tripStops[index]),
     );
   }
 }
