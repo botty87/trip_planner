f5721f21e8cb28c5cf35bedb35f28804f1dda877
marco bottichio <marco.bottichio@pm.me>
2024-01-26 14:35:53 +0100

    day_trip_card_test added

---
+++ test/features/trips/presentation/widgets/trip_page/day_trip_card_test.dart
@@ -0,0 +1,42 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_logger/easy_logger.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:implicitly_animated_reorderable_list_2/implicitly_animated_reorderable_list_2.dart';
+import 'package:trip_planner/core/widgets/trip/generic_trip_card.dart';
+import 'package:trip_planner/features/day_trips/domain/entities/day_trip.dart';
+import 'package:trip_planner/features/trips/presentation/widgets/trip_page/day_trip_card.dart';
+
+import '../../../../../utils.dart';
+
+void main() {
+  const tDayTrip = DayTrip(index: 0, description: "description");
+  final tTripStartDate = DateTime.now();
+
+  setUpAll(() async {
+    EasyLocalization.logger.enableLevels = [LevelMessages.error, LevelMessages.debug];
+  });
+
+  testWidgets('renders DayTripCard that conains GenericTripCard', (tester) async {
+    await tester.pumpWidget(
+      TestUtils.defaultWidget(
+        ImplicitlyAnimatedReorderableList<DayTrip>(
+          items: const [tDayTrip],
+          areItemsTheSame: (oldItem, newItem) => oldItem == newItem,
+          itemBuilder: (context, animation, item, i) {
+            return Reorderable(
+              key: const Key("reorderable"),
+              child: DayTripCard(
+                dayTrip: tDayTrip,
+                tripStartDate: tTripStartDate,
+              ),
+            );
+          },
+          onReorderFinished: (item, from, to, newItems) {},
+        ),
+      ),
+    );
+
+    expect(find.byType(GenericTripCard), findsOneWidget);
+  });
+}
