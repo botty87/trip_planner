84d3683209f8974167d0a935831d540ee5191ca2
marco bottichio <marco.bottichio@pm.me>
2024-01-26 14:22:24 +0100

    add_day_trip_card_test

--- test/features/trips/presentation/widgets/trip_page/add_day_trip_card_test.dart
+++ test/features/trips/presentation/widgets/trip_page/add_day_trip_card_test.dart
@@ -2,8 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:responsive_framework/breakpoint.dart';
-import 'package:responsive_framework/responsive_breakpoints.dart';
 import 'package:trip_planner/core/widgets/trip/add_destination_card.dart';
 import 'package:trip_planner/features/trips/presentation/widgets/trip_page/add_day_trip_card.dart';
 
@@ -16,7 +14,7 @@ void main() {
 
   testWidgets('renders AddDayTripCard that conains AddDestinationCard', (tester) async {
     await tester.pumpWidget(
-      TestUtils.defaultWidget(AddDayTripCard()),
+      TestUtils.defaultWidget(const SingleChildScrollView(child: AddDayTripCard())),
     );
 
     expect(find.byType(AddDestinationCard), findsOneWidget);
