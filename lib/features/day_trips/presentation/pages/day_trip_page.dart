540e5d5e2fd27b30f18c37c02f6a929301f59400
marco bottichio <marco.bottichio@pm.me>
2024-01-29 23:11:43 +0100

    implemented day trip error widget

--- lib/features/day_trips/presentation/pages/day_trip_page.dart
+++ lib/features/day_trips/presentation/pages/day_trip_page.dart
@@ -7,19 +7,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../../../core/constants.dart';
 import '../../../../core/di/di.dart';
+import '../../../../core/error/exceptions.dart';
 import '../../../../core/l10n/locale_keys.g.dart';
-import '../../../../core/routes/app_router.gr.dart';
-import '../../../../core/utilities/pair.dart';
 import '../../../../core/widgets/snackbars.dart';
-import '../../../../core/widgets/trip/generic_trip_card.dart';
 import '../../../../core/widgets/trip/save_cancel_edit_buttons.dart';
 import '../../../../core/widgets/trip_pages_animated_switcher.dart';
-import '../../../trip_stops/domain/entities/trip_stop.dart';
 import '../../../trips/domain/entities/trip.dart';
 import '../../domain/entities/day_trip.dart';
 import '../../domain/entities/trip_stops_directions.dart';
@@ -27,6 +23,7 @@ import '../cubit/day_trip/day_trip_cubit.dart';
 import '../cubit/trip_stops_map/trip_stops_map_cubit.dart';
 import '../widgets/day_trip_page/day_trip_page_initial_widget.dart';
 import '../widgets/day_trip_page/day_trip_page_loaded.dart';
+import '../widgets/day_trip_page/day_trip_error_widget.dart';
 
 part '../widgets/day_trip_page/list_tab/save_cancel_edit_buttons.dart';
 part '../widgets/day_trip_page/map_tab/map_directions_loader.dart';
@@ -35,7 +32,7 @@ part '../widgets/day_trip_page/map_tab/map_view_widget.dart';
 part '../widgets/day_trip_page/map_tab/map_widget.dart';
 
 @RoutePage()
-class DayTripPage extends StatelessWidget {
+class DayTripPage extends HookWidget {
   final Trip _trip;
   final DayTrip _dayTrip;
 
@@ -45,6 +42,8 @@ class DayTripPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isModalBottomEditing = useRef<bool>(false);
+
     return MultiBlocProvider(
       providers: [
         BlocProvider<DayTripCubit>(
@@ -75,49 +74,67 @@ class DayTripPage extends StatelessWidget {
               length: 2,
               child: Scaffold(
                 appBar: _buildAppBar(context),
-                body: Builder(builder: (context) {
-                  return NotificationListener(
-                    onNotification: (notification) {
-                      if (notification is ScrollEndNotification) {
-                        final tabIndex = DefaultTabController.of(context).index;
-                        context.read<TripStopsMapCubit>().selectTab(tabIndex == 1);
-                      }
-                      return true;
-                    },
-                    child: BlocBuilder<DayTripCubit, DayTripState>(
-                      buildWhen: (previous, current) => current.maybeMap(
-                        deleting: (_) => false,
-                        error: (state) => state.fatal,
-                        deleted: (_) => false,
-                        orElse: () =>
-                            previous.runtimeType != current.runtimeType &&
-                            previous.maybeMap(
-                              error: (value) => value.fatal,
-                              orElse: () => true,
-                            ) &&
-                            current.maybeMap(
-                              editing: (_) => false,
-                              orElse: () => true,
-                            ),
+                body: MultiBlocListener(
+                  listeners: [
+                    //Show snackbar when error is not fatal and is not editing
+                    BlocListener<DayTripCubit, DayTripState>(
+                      listenWhen: (previous, current) => current.maybeMap(
+                        error: (state) => !state.fatal && isModalBottomEditing.value == false,
+                        orElse: () => false,
                       ),
-                      builder: (context, state) {
-                        return TripPagesAnimatedSwitcher(
-                          child: state.maybeMap(
-                            initial: (_) =>
-                                const DayTripPageInitialWidget(key: ValueKey('initial')),
-                            loaded: (_) =>
-                                const Center(key: ValueKey('loaded'), child: DayTripPageLoaded()),
-                            /* error: (state) => Center(
-                              key: const ValueKey('error'),
-                              child: TripErrorWidget(message: state.errorMessage),
-                            ), */
-                            orElse: () => throw UnimplementedError(),
-                          ),
+                      listener: (context, state) {
+                        final errorMessage = state.maybeMap(
+                          error: (state) => state.errorMessage,
+                          orElse: () => throw UnexpectedException(),
                         );
+                        ScaffoldMessenger.of(context).showSnackBar(Snackbars.error(errorMessage));
                       },
                     ),
-                  );
-                }),
+                  ],
+                  child: Builder(builder: (context) {
+                    return NotificationListener(
+                      onNotification: (notification) {
+                        if (notification is ScrollEndNotification) {
+                          final tabIndex = DefaultTabController.of(context).index;
+                          context.read<TripStopsMapCubit>().selectTab(tabIndex == 1);
+                        }
+                        return true;
+                      },
+                      child: BlocBuilder<DayTripCubit, DayTripState>(
+                        buildWhen: (previous, current) => current.maybeMap(
+                          deleting: (_) => false,
+                          error: (state) => state.fatal,
+                          deleted: (_) => false,
+                          orElse: () =>
+                              previous.runtimeType != current.runtimeType &&
+                              previous.maybeMap(
+                                error: (value) => value.fatal,
+                                orElse: () => true,
+                              ) &&
+                              current.maybeMap(
+                                editing: (_) => false,
+                                orElse: () => true,
+                              ),
+                        ),
+                        builder: (context, state) {
+                          return TripPagesAnimatedSwitcher(
+                            child: state.maybeMap(
+                              initial: (_) =>
+                                  const DayTripPageInitialWidget(key: ValueKey('initial')),
+                              loaded: (_) =>
+                                  const Center(key: ValueKey('loaded'), child: DayTripPageLoaded()),
+                              error: (state) => Center(
+                                key: const ValueKey('error'),
+                                child: DayTripErrorWidget(message: state.errorMessage),
+                              ),
+                              orElse: () => throw UnimplementedError(),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  }),
+                ),
               ),
             ),
           );
---
+++ lib/features/day_trips/presentation/widgets/day_trip_page/day_trip_error_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../../core/widgets/generic_error_widget.dart';
+import '../../cubit/day_trip/day_trip_cubit.dart';
+
+class DayTripErrorWidget extends StatelessWidget {
+  final String message;
+  const DayTripErrorWidget({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return GenericErrorWidget(
+      message: message,
+      onRetry: () => context.read<DayTripCubit>()
+        ..startListenTripStops()
+        ..startListenDayTrip(),
+    );
+  }
+}
