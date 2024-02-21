f2a02262684928c4185df381884505d850448b25
marco bottichio <marco.bottichio@pm.me>
2024-02-21 19:22:42 +0100

    code clean

--- lib/features/trips/presentation/widgets/new_edit_trip_form/new_edit_trip_form.dart
+++ lib/features/trips/presentation/widgets/new_edit_trip_form/new_edit_trip_form.dart
@@ -1,6 +1,4 @@
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
-import 'package:collection/collection.dart';
-import 'package:devicelocale/devicelocale.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
