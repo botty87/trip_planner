20abceae515bc4ab7b4d33b60033b906facde1cc
marco bottichio <marco.bottichio@pm.me>
2024-03-08 18:58:05 +0100

    save_cancel_edit_buttons_test

--- test/features/trips/presentation/widgets/trip_page/save_cancel_edit_buttons_test.dart
+++ test/features/trips/presentation/widgets/trip_page/save_cancel_edit_buttons_test.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:trip_planner/features/ui/presentation/widgets/trip/save_cancel_edit_buttons.dart';
 import 'package:trip_planner/features/trips/presentation/widgets/trip_page/save_cancel_edit_buttons.dart';
 
@@ -14,7 +15,7 @@ void main() {
   testWidgets('renders SaveCancelEditButtons that contains GenericSaveCancelEditButtons',
       (tester) async {
     await tester.pumpWidget(TestUtils.defaultWidget(
-      SaveCancelEditButtons(
+      child: SaveCancelEditButtons(
         isSaving: const Stream.empty(),
         onCancel: () {},
         onSave: () {},
@@ -23,5 +24,30 @@ void main() {
     ));
 
     expect(find.byType(GenericSaveCancelEditButtons), findsOneWidget);
+  });
+
+  //Golden test
+  testGoldens('renders SaveCancelEditButtons that contains GenericSaveCancelEditButtons',
+      (tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: [
+        Device.phone,
+        Device.iphone11,
+        Device.tabletLandscape,
+        Device.tabletPortrait,
+      ])
+      ..addScenario(
+        name: 'SaveCancelEditButtons',
+        widget: SaveCancelEditButtons(
+          isSaving: const Stream.empty(),
+          onCancel: () {},
+          onSave: () {},
+          errorMessage: const Stream.empty(),
+        ),
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+
+    await screenMatchesGolden(tester, 'save_cancel_edit_buttons');
   });
 }
