cf461a1f2bfe16fecd45382c006a7cfa8dff5213
marco bottichio <marco.bottichio@pm.me>
2024-03-08 20:31:41 +0100

    Update day_trip_page_initial_widget_test.dart

--- test/features/day_trips/presentation/widgets/day_trip_page/day_trip_page_initial_widget_test.dart
+++ test/features/day_trips/presentation/widgets/day_trip_page/day_trip_page_initial_widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:trip_planner/features/ui/presentation/widgets/generics/generic_initial_widget.dart';
 import 'package:trip_planner/features/day_trips/presentation/widgets/day_trip_page/day_trip_page_initial_widget.dart';
@@ -7,10 +8,20 @@ import '../../../../../utils.dart';
 void main() {
   testWidgets('should show GenericInitialWidget', (WidgetTester tester) async {
     await tester.runAsync(() async {
-      await tester.pumpWidget(TestUtils.defaultWidget(const DayTripPageInitialWidget()));
+      await tester.pumpWidget(TestUtils.defaultWidget(child: const DayTripPageInitialWidget()));
 
       expect(find.byType(DayTripPageInitialWidget), findsOneWidget);
       expect(find.byType(GenericInitialWidget), findsOneWidget);
+    });
+  });
+
+  testWidgets('renders CircularProgressIndicator', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      await tester.pumpWidget(TestUtils.defaultWidget(child: const DayTripPageInitialWidget()));
+      await Future.delayed(const Duration(milliseconds: 500));
+      await tester.pump();
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
     });
   });
 }
