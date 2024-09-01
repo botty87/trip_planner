ddf0efaee9afb73c88671169584a0c73f1726ea3
marco bottichio <marco.bottichio@pm.me>
2024-09-01 16:04:41 +0200

    Enhance DayTripDescription layout with conditional spacing

--- lib/features/day_trips/presentation/widgets/day_trip_page/list_tab/day_trip_description.dart
+++ lib/features/day_trips/presentation/widgets/day_trip_page/list_tab/day_trip_description.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../../core/constants.dart';
 import '../../../../../../ui/widgets/trip/generic_trip_description.dart';
 import '../../../cubit/day_trip/day_trip_cubit.dart';
 
@@ -11,6 +12,13 @@ final class DayTripDescription extends StatelessWidget {
   Widget build(BuildContext context) {
     final description = context.select((DayTripCubit cubit) => cubit.state.dayTrip.description);
 
-    return GenericTripDescription(description: description);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (description?.isNotEmpty ?? false) const SizedBox(height: verticalSpaceS),
+        GenericTripDescription(description: description),
+        if (description?.isNotEmpty ?? false) const SizedBox(height: verticalSpace),
+      ],
+    );
   }
 }
