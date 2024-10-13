4e3def3d732c113630012e74e615c0bde325cb37
marco bottichio <marco.bottichio@pm.me>
2024-10-13 15:41:27 +0200

    refactor: implement OrientationBuilder in NoTripsWidget for responsive layout adjustments based on device orientation

--- lib/features/trips/presentation/widgets/trips_page/no_trips_widget.dart
+++ lib/features/trips/presentation/widgets/trips_page/no_trips_widget.dart
@@ -7,8 +7,9 @@ import 'package:vector_graphics/vector_graphics.dart';
 import '../../../../../core/constants.dart';
 import '../../../../../core/l10n/locale_keys.g.dart';
 import '../../../../../gen/assets.gen.dart';
-import '../../../../ads/presentation/widgets/native_ad.dart';
 import '../../../../../ui/widgets/background/background_widget_container.dart';
+import '../../../../../ui/widgets/generics/gap.dart';
+import '../../../../ads/presentation/widgets/native_ad.dart';
 
 class NoTripsWidget extends StatelessWidget {
   const NoTripsWidget({super.key});
@@ -17,46 +18,57 @@ class NoTripsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       minimum: const EdgeInsets.all(verticalSpaceL),
-      child: Column(
-        children: [
-          NativeAd.trips(),
-          const SizedBox(height: verticalSpaceS),
-          Expanded(
-            child: BackgroundWidgetContainer(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Center(
-                    child: Text(
-                      LocaleKeys.noTripAddOne.tr(),
-                      style: GoogleFonts.caveat(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
+      child: OrientationBuilder(builder: (context, orientation) {
+        return Flex(
+          direction: orientation == Orientation.portrait ? Axis.vertical : Axis.horizontal,
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight:
+                    orientation == Orientation.portrait ? MediaQuery.of(context).size.height * 0.4 : double.infinity,
+                maxWidth:
+                    orientation == Orientation.landscape ? MediaQuery.of(context).size.width * 0.4 : double.infinity,
+              ),
+              child: NativeAd.trips(),
+            ),
+            const Gap(verticalSpaceS),
+            Expanded(
+              child: BackgroundWidgetContainer(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Center(
+                      child: Text(
+                        LocaleKeys.noTripAddOne.tr(),
+                        style: GoogleFonts.caveat(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  Expanded(
-                    child: SvgPicture(
-                      AssetBytesLoader(Assets.svg.tripSvg),
-                    ),
-                  ),
-                  Transform.rotate(
-                    angle: 160,
-                    child: Transform.flip(
-                      flipX: true,
+                    Expanded(
                       child: SvgPicture(
-                        AssetBytesLoader(Assets.svg.arrowSvg),
-                        height: 150,
+                        AssetBytesLoader(Assets.svg.tripSvg),
                       ),
                     ),
-                  ),
-                ],
+                    Transform.rotate(
+                      angle: 160,
+                      child: Transform.flip(
+                        flipX: true,
+                        child: SvgPicture(
+                          AssetBytesLoader(Assets.svg.arrowSvg),
+                          height: 150,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        );
+      }),
     );
   }
 }
