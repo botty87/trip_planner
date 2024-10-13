c899514b4db50f424b95367cee3bdc09582ad7c9
marco bottichio <marco.bottichio@pm.me>
2024-10-13 15:55:59 +0200

    refactor: streamline NoTripsWidget layout using RowColumn and adaptive widgets for better responsiveness to device orientation

--- lib/features/trips/presentation/widgets/trips_page/no_trips_widget.dart
+++ lib/features/trips/presentation/widgets/trips_page/no_trips_widget.dart
@@ -7,8 +7,9 @@ import 'package:vector_graphics/vector_graphics.dart';
 import '../../../../../core/constants.dart';
 import '../../../../../core/l10n/locale_keys.g.dart';
 import '../../../../../gen/assets.gen.dart';
+import '../../../../../ui/widgets/adaptive/gap.dart';
+import '../../../../../ui/widgets/adaptive/row_column.dart';
 import '../../../../../ui/widgets/background/background_widget_container.dart';
-import '../../../../../ui/widgets/generics/gap.dart';
 import '../../../../ads/presentation/widgets/native_ad.dart';
 
 class NoTripsWidget extends StatelessWidget {
@@ -18,57 +19,56 @@ class NoTripsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       minimum: const EdgeInsets.all(verticalSpaceL),
-      child: OrientationBuilder(builder: (context, orientation) {
-        return Flex(
-          direction: orientation == Orientation.portrait ? Axis.vertical : Axis.horizontal,
-          children: [
-            ConstrainedBox(
-              constraints: BoxConstraints(
-                maxHeight:
-                    orientation == Orientation.portrait ? MediaQuery.of(context).size.height * 0.4 : double.infinity,
-                maxWidth:
-                    orientation == Orientation.landscape ? MediaQuery.of(context).size.width * 0.4 : double.infinity,
-              ),
-              child: NativeAd.trips(),
+      child: RowColumn(
+        children: [
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).orientation == Orientation.portrait
+                  ? MediaQuery.of(context).size.height * 0.4
+                  : double.infinity,
+              maxWidth: MediaQuery.of(context).orientation == Orientation.landscape
+                  ? MediaQuery.of(context).size.width * 0.4
+                  : double.infinity,
             ),
-            const Gap(verticalSpaceS),
-            Expanded(
-              child: BackgroundWidgetContainer(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Center(
-                      child: Text(
-                        LocaleKeys.noTripAddOne.tr(),
-                        style: GoogleFonts.caveat(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
+            child: NativeAd.trips(),
+          ),
+          const Gap(verticalSpaceS),
+          Expanded(
+            child: BackgroundWidgetContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Center(
+                    child: Text(
+                      LocaleKeys.noTripAddOne.tr(),
+                      style: GoogleFonts.caveat(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    Expanded(
+                  ),
+                  Expanded(
+                    child: SvgPicture(
+                      AssetBytesLoader(Assets.svg.tripSvg),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: 160,
+                    child: Transform.flip(
+                      flipX: true,
                       child: SvgPicture(
-                        AssetBytesLoader(Assets.svg.tripSvg),
+                        AssetBytesLoader(Assets.svg.arrowSvg),
+                        height: 150,
                       ),
                     ),
-                    Transform.rotate(
-                      angle: 160,
-                      child: Transform.flip(
-                        flipX: true,
-                        child: SvgPicture(
-                          AssetBytesLoader(Assets.svg.arrowSvg),
-                          height: 150,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        );
-      }),
+          ),
+        ],
+      ),
     );
   }
 }
