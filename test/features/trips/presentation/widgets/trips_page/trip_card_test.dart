4a30571ef0c7776d2327f575328125e6b8b29ac4
marco bottichio <marco.bottichio@pm.me>
2025-01-17 19:35:28 +0100

    test: add golden tests and images for TripCard to verify rendering with different trip states

--- test/features/trips/presentation/widgets/trips_page/trip_card_test.dart
+++ test/features/trips/presentation/widgets/trips_page/trip_card_test.dart
@@ -1,3 +1,4 @@
+import 'package:alchemist/alchemist.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -31,5 +32,24 @@ void main() {
     expect($('Trip Name'), findsOneWidget);
     expect($('Trip Description'), findsOneWidget);
     expect($("${LocaleKeys.createdOn.tr()} ${DateFormat.yMMMMd().format(trip.createdAt)}"), findsOneWidget);
+  });
+
+  goldenTest('renders TripCard with correct trip details', fileName: 'trip_card', builder: () {
+    return GoldenTestGroup(
+      children: [
+        GoldenTestScenario(
+          name: 'with no description',
+          child: TripCard(trip: trip.copyWith(description: null), fillDescription: false),
+        ),
+        GoldenTestScenario(
+          name: 'with description',
+          child: TripCard(trip: trip, fillDescription: false),
+        ),
+        GoldenTestScenario(
+          name: 'with public trip',
+          child: TripCard(trip: trip.copyWith(isPublic: true), fillDescription: false),
+        ),
+      ],
+    );
   });
 }
