99c87ac99ade295a13778f875a85a78b4f87814f
marco bottichio <marco.bottichio@pm.me>
2025-01-18 22:36:51 +0100

    test: add golden tests and images for SaveCancelEditButtons to verify rendering and ensure visual consistency

--- test/features/trips/presentation/widgets/trip_page/save_cancel_edit_buttons_test.dart
+++ test/features/trips/presentation/widgets/trip_page/save_cancel_edit_buttons_test.dart
@@ -1,6 +1,10 @@
+import 'package:alchemist/alchemist.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_logger/easy_logger.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+import 'package:trip_planner/core/l10n/locale_keys.g.dart';
 import 'package:trip_planner/features/trips/presentation/widgets/trip_page/save_cancel_edit_buttons.dart';
 import 'package:trip_planner/ui/widgets/trip/save_cancel_edit_buttons.dart';
 
@@ -11,8 +15,8 @@ void main() {
     EasyLocalization.logger.enableLevels = [LevelMessages.error, LevelMessages.debug];
   });
 
-  testWidgets('renders SaveCancelEditButtons that contains GenericSaveCancelEditButtons', (tester) async {
-    await tester.pumpWidget(TestUtils.defaultWidget(
+  patrolWidgetTest('renders SaveCancelEditButtons that contains GenericSaveCancelEditButtons', ($) async {
+    await $.pumpWidget(TestUtils.defaultWidget(
       child: SaveCancelEditButtons(
         isSaving: const Stream.empty(),
         onCancel: () {},
@@ -21,6 +25,27 @@ void main() {
       ),
     ));
 
-    expect(find.byType(GenericSaveCancelEditButtons), findsOneWidget);
+    expect($(GenericSaveCancelEditButtons), findsOneWidget);
+    expect($(LocaleKeys.cancel.tr()), findsOneWidget);
+    expect($(LocaleKeys.save.tr()), findsOneWidget);
   });
+
+  goldenTest(
+    'renders SaveCancelEditButtons that contains GenericSaveCancelEditButtons',
+    fileName: 'save_cancel_edit_buttons',
+    builder: () => GoldenTestGroup(
+      scenarioConstraints: const BoxConstraints(minWidth: 200),
+      children: [
+        GoldenTestScenario(
+          name: 'normal',
+          child: SaveCancelEditButtons(
+            isSaving: const Stream.empty(),
+            onCancel: () {},
+            onSave: () {},
+            errorMessage: const Stream.empty(),
+          ),
+        ),
+      ],
+    ),
+  );
 }
