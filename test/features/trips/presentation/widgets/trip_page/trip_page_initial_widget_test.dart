be18cb8c832a56f683c3e2a402ab641a6a0c95da
marco bottichio <marco.bottichio@pm.me>
2025-01-18 22:47:16 +0100

    test: refactor TripPageInitialWidget test to use patrol for improved interaction and maintainability

--- test/features/trips/presentation/widgets/trip_page/trip_page_initial_widget_test.dart
+++ test/features/trips/presentation/widgets/trip_page/trip_page_initial_widget_test.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
 import 'package:trip_planner/ui/widgets/generics/generic_initial_widget.dart';
 import 'package:trip_planner/features/trips/presentation/widgets/trip_page/trip_page_initial_widget.dart';
 
 import '../../../../../utils.dart';
 
 void main() {
-  testWidgets('TripPageInitialWidget has a GenericInitialWidget', (WidgetTester tester) async {
-    await tester.runAsync(() {
-      return tester.pumpWidget(TestUtils.defaultWidget(child: const TripPageInitialWidget()));
+  patrolWidgetTest('TripPageInitialWidget has a GenericInitialWidget', ($) async {
+    await $.tester.runAsync(() {
+      return $.pumpWidget(TestUtils.defaultWidget(child: const TripPageInitialWidget()));
     });
 
     expect(find.byType(GenericInitialWidget), findsOneWidget);
